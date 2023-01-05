8a69a34569b5d2c40499fc179ddeeed3961a1b52
Vibhuti <vibhuti6378@gmail.com>
2023-01-05 23:34:18 +0530

    Changes made

--- src/components/Images.js
+++ src/components/Images.js
@@ -2,15 +2,29 @@ import { useContext } from "react";
 import { ImageContext } from "../App";
 import Image from "./Image";
 import Skeleton from "./Skeleton";
+import None from "./None";
 const Images = () =>{
 
     const { response, isLoading, searchImage} = useContext(ImageContext);
     return(
         <>
             <h1 className="text-center mt-6 underline text-2xl">Results for {searchImage || 'Coffee'}</h1>
-                <div className="grid md: grid-cols-1 lg:grid-cols-3 xl:grid-cols-3 gap-4 my-10 max-w-7xl mx-auto px=4">
-                    {isLoading ? <Skeleton item={12}/>:response.map((data,key)=> <Image key={key} data={data}/>)}
-                </div>
+            {isLoading?<div
+                className="grid md:grid-cols-1 lg:grid-cols-3 xl:grid-cols-3 gap-4 max-w-7xl mx-auto  px-4"
+                > <Skeleton item={30} />
+                </div>:
+                response && response.length!==0?
+                <div
+                className="grid md:grid-cols-1 lg:grid-cols-3 xl:grid-cols-3 gap-4 max-w-7xl mx-auto px-4"
+                >
+                {response.map((data,key)=>
+                   <Image key={key} data={data}/>
+                
+               )
+                }
+               </div>
+                 :
+                <None />}
         </>
     )
 }
---
+++ src/components/None.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const None=()=>{
+    return(
+    <div>
+    <div className="text-center mt-1 font-bold text-2xl">Sorry! No Images Found</div>
+    </div>
+    );
+}
+
+export default None;
